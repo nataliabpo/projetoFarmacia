25fd69097aab9c6a4ed08fcc8e0780ebbe7b1ab2
nataliabpo <nataliaoliveira0108@gmail.com>
2024-02-28 13:29:13 -0300

    atualização

--- projetoFarmacia/src/components/navBar/NavBar.tsx
+++ projetoFarmacia/src/components/navBar/NavBar.tsx
@@ -11,6 +11,7 @@ function Navbar() {
             <div className='text-2xl font-bold uppercase'>Farmacia Generation</div>
 
             <div className='flex gap-4'>
+            <Link to='/home' className='hover:underline'>Home</Link>
             <Link to='/categorias' className='hover:underline'>Categorias</Link>
             <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar Categoria</Link>
               <div className='hover:underline'>Produtos</div>
