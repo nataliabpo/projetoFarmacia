4a5bbda627d53fd470858199deb402a87edad3f8
nataliabpo <nataliaoliveira0108@gmail.com>
2024-02-28 12:09:13 -0300

    crud categoria

--- projetoFarmacia/src/components/categoria/cardCategoria/CardCategoria.tsx
+++ projetoFarmacia/src/components/categoria/cardCategoria/CardCategoria.tsx
@@ -1,108 +1,37 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { atualizar, buscar, cadastrar } from "../../../service/Service";
-import { ChangeEvent, useState } from "react";
-import Categoria from "../../../models/Categoria";
+import { Link } from "react-router-dom"
+import Categoria from "../../../models/Categoria"
 
-function FormularioCategoria() {
-    const navigate = useNavigate();
+interface CardTemasProps {
+    categoria: Categoria
+}
 
-    const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-
-    {/** useParams é um Hook do React Router que permite acessar parâmatros passados na URL */ }
-    const { id } = useParams<{ id: string }>();
-
-    async function buscarPorId(id: string) {
-        try {
-            await buscar(`/categoria/${id}`, setCategoria)
-        } catch (error: any) {
-            alert("Erro ao buscar pela Categoria")
-            console.log(error);
-        }
-    }
-
-    useEffect(() => {
-        if (id !== undefined) {
-            buscarPorId(id)
-        }
-    }, [id])
-
-    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-        setCategoria({
-            ...categoria,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    function retornar() {
-        navigate("/home")
-    }
-
-    async function gerarNovaCategoria(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault()
-        setIsLoading(true)
-
-        if (id !== undefined) {
-            try {
-                await atualizar(`/categoria`, categoria, setCategoria)
-                alert('A Categoria foi cadastrada com sucesso!')
-            } catch (error: any) {
-                alert("Erro ao buscar pela Categoria")
-                console.log(error);
-            }
-        } else {
-            try {
-                await cadastrar(`/categoria`, categoria, setCategoria)
-                alert('A Categoria foi atualizada com sucesso!')
-            } catch (error: any) {
-                alert("Erro ao buscar pela Categoria")
-                console.log(error);
-            }
-        }
-
-        setIsLoading(false)
-        retornar()
-    }
-
+function CardTemas({ categoria }: CardTemasProps) {
     return (
-        <div className="container flex flex-col items-center justify-center mx-auto">
-            <h1 className="text-4xl text-center my-8">
-                {id === undefined ? 'Cadastrar Categoria' : 'Editar Categoria'}
-            </h1>
+        <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
+            <header className='py-2 px-6 bg-[#5A1F21] text-white font-bold text-2xl'>
+                Categoria
+            </header>
 
-            <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovaCategoria}>
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="descricao">Descrição da Categoria</label>
-                    <input
-                        type="text"
-                        placeholder="Descreva aqui sua categoria"
-                        name='nome'
-                        className="border-2 border-slate-700 rounded p-2"
-                        value={categoria.nome}
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                </div>
-                <button
-                    className="rounded text-slate-100 bg-indigo-400 
-                               hover:bg-indigo-800 w-1/2 py-2 mx-auto flex justify-center"
-                    type="submit">
+            <p className='p-8 text-3xl bg-slate-200 h-full'>
+                {categoria.nome}
+            </p>
 
-                    {isLoading ?
-                        <RotatingLines
-                            strokeColor="white"
-                            strokeWidth="5"
-                            animationDuration="0.75"
-                            width="24"
-                            visible={true}
-                        /> :
-                        <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
-                        
-                    }
-                    
-                </button>
-            </form>
+            <div className="flex">
+                <Link to={`/editarCategory/${categoria.id}`}
+                    className='w-full text-slate-100 bg-[#c19e9f] hover:bg-[#f67b7f] 
+                        flex items-center justify-center py-2'>
+                    <button>Editar</button>
+                </Link>
+
+                <Link to={`/deletarCategory/${categoria.id}`}
+                    className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
+                        flex items-center justify-center'>
+                    <button>Deletar</button>
+                </Link>
+            </div>
+
         </div>
     )
 }
 
-export default FormularioCategoria;
+export default CardTemas
